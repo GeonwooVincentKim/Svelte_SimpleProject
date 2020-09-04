4c294e92f26b429c5fdbea4c0045943d9c2c19aa
GeonwooVincentKim <kdsnop@gmail.com>
2020-09-04 11:11:28 +0900

    Created basic form of this project.

--- src/App.svelte
+++ src/App.svelte
@@ -8,21 +8,29 @@
 	$: fullName = `${FirstName} ${LastName}`;
 	$: {console.log(beltColour); console.log(fullName);}
 
-	let imgSrc = "images2.png";
+	let imgSrc = "images.png";
+	let imgSrc2 = "images2.png";
 </script>
 
 <main>
 	<h1>Hello {fullName}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+	<!-- Output full-name here. -->
+	<p style="color: {beltColour}">{fullName} - has {beltColour} belt</p>
+
+	<!-- Two way bounding, 'handleInput' and 'beltColour'. -->
+	<!-- <input type="text" on:input={handleInput} value={beltColour}> -->
+	
+	<input type="text" bind:value={FirstName}>
+	<input type="text" bind:value={LastName}>
+	<!-- 
+		One way bounding, but it controlls all of things 
+		by using 'beltColour'. 
+	-->
+	<input type="text" bind:value={beltColour}>
+
+	<!-- svelte-ignore a11y-missing-attribute -->
+	<img src={imgSrc}>
+
 </main>
-
-<style>
-	main { text-align: center; padding: 1em; max-width: 240px; margin: 0 auto; }
-h1 { color: #ff3e00; text-transform: uppercase; font-size: 4em; font-weight: 100; }
-
-@media (min-width: 640px) {
-    main {
-        max-width: none;
-    }
-}
-</style>
